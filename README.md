fadb4aa657bb0adf5097f7b8deba53779a5bddc6
Jilin <jilin@bu.edu>
2024-02-15 21:55:51 -0500

    script should be correctly set up, but uncertain if queries are correct/what we want

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # NISLAB K8s Autoscaling Research
 
+## Lab Machine Programs & Ports
+
+- Prometheus: http://localhost:30000
+- Alertmanager: http://localhost:31000
+- Grafana: http://localhost:32000/login
+
 ## References
+
 - [Monitoring Stack Setup/K8s Tutorial](https://devopscube.com/kubernetes-tutorials-beginners/)
 - [Prometheus HTTP API](https://prometheus.io/docs/prometheus/latest/querying/api/)
 - [PromQL Tutorial](https://valyala.medium.com/promql-tutorial-for-beginners-9ab455142085)
 - [PromQL Cheat Sheet](https://promlabs.com/promql-cheat-sheet/)
 - [Python JSON to CSV](https://blog.enterprisedna.co/python-convert-json-to-csv/)
+- [K8s Monitoring Repo](https://github.com/camilb/prometheus-kubernetes)
+- [Resource Usage Queries](https://stackoverflow.com/questions/40327062/how-to-calculate-containers-cpu-usage-in-kubernetes-with-prometheus-as-monitori)
--- export_prom_metrics.py
+++ export_prom_metrics.py
@@ -6,23 +6,22 @@ import pandas as pd
 def main():
     '''Query Prometheus HTTP API for specified metrics, and convert the json response into a csv file.'''
 
-    # Get json response from HTTP API
+    # Get json responses from Prometheus HTTP API
     prom_api = "http://localhost:30000/api/v1/query"
-    test_response = requests.get(prom_api, params={"query": "container_cpu_user_seconds_total"})
-    data = test_response.json()
-    
+    pod_response = requests.get(prom_api, params={"query": "rate(container_cpu_usage_seconds_total{image='docker.io/library/nginx:latest'}[5m])"}).json()
+    cluster_response = requests.get(prom_api, params={"query": "sum (rate (container_cpu_usage_seconds_total{id='/'}[1m])) / sum (machine_cpu_cores) * 100"}).json()
+
     # Write json data to a json file
-    with open("test_json.json", 'w') as json_file:
-        json.dump(data, json_file)
-    #test_json = json.load(test_response)
-    #print(type(test_json))
+    with open("pod_response.json", 'w') as json_file:
+        json.dump(pod_response, json_file, indent=4)
+    with open("cluster_response.json", 'w') as json_file:
+        json.dump(cluster_response, json_file, indent=4)
 
     # Convert json to csv
-    
-    test_pandas_dataframe = pd.read_json("test_json.json")
-    print(test_pandas_dataframe)
-    test_pandas_dataframe.to_csv("output.csv", index=False)
-    
+    test_pandas_dataframe = pd.read_json("pod_response.json")
+    test_pandas_dataframe.to_csv("pod_response_output.csv", index=False)
+    test_pandas_dataframe = pd.read_json("cluster_response.json")
+    test_pandas_dataframe.to_csv("cluster_response_output.csv", index=False)
 
 
 if __name__ == "__main__":
