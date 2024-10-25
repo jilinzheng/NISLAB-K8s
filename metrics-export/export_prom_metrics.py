e1832ab832db8bd42fe78c808df88b940e4bd283
Jilin Zheng <jilin@bu.edu>
2024-10-24 23:11:20 -0400

    update metrics export so export service units, began custom metrics implementation...

--- metrics-export/export_prom_metrics.py
+++ metrics-export/export_prom_metrics.py
@@ -2,25 +2,27 @@
 Query Prometheus HTTP API for specified metrics
 """
 
-
 import requests
-import time
 from datetime import datetime
 import pandas as pd
-
 
 # Prometheus APIs
 PROM_QUERY_INSTANT_API = "http://127.0.0.1:30000/api/v1/query"
 PROM_QUERY_RANGE_API = "http://127.0.0.1:30000/api/v1/query_range"
 
 # start and end times of test result range
-START_TIME = ''      # specify unix start time
-END_TIME = ''        # specify end time
-
+START_TIME = '1729299876'      # specify unix start time
+END_TIME = '1729310683'        # specify end time
+EXPORT_FILENAME = '241018_cust_delay_const_attk_7.5x_3h_v3.csv'
 
 # get the container cpu usage per pod
 params={"query":
-        "sum(rate(container_cpu_usage_seconds_total{namespace='default'}[1m])) by (pod)",
+        """ceil(
+                max(
+                    sum(rate(container_cpu_usage_seconds_total{namespace="default"}[1m])) or
+                    sum(container_memory_working_set_bytes{namespace='default'} / (4 * 1024 * 1024 * 1024))
+                )
+            )""",
         "start":
         f'{START_TIME}',
         "end":
@@ -30,27 +32,18 @@ params={"query":
 }
 res = requests.get(PROM_QUERY_RANGE_API, params=params).json()
 
+# extract desired data: max service units by the minute
+max_service_unit_values = res['data']['result'][0]['values']
+timestamps, max_service_units = [], []
+for value in max_service_unit_values:
+    timestamp = value[0]
+    timestamps.append(datetime.fromtimestamp(timestamp))
+    max_service_units.append(value[1])
 
-# extract desired data: podname, timestamp, and usage value
-res_data = res['data']['result']
-pods, timestamps, values = [], [], []
-for pod in res_data:
-    pod_name = pod['metric']['pod']
-    first_entry = True
-    for timestamp, value in pod['values']:
-        if first_entry:
-            pods.append(pod_name)
-            first_entry = False
-        else:
-            pods.append('')
-        timestamps.append(datetime.fromtimestamp(timestamp))
-        values.append(float(value))
-
-
-# create dataframe + write to excel
+# create dataframe + write to csv
 df = pd.DataFrame({
-    'Pod': pods,
     'Timestamp': timestamps,
-    'Value': values
+    'Max Service Units': max_service_units
 })
-df.to_excel('./prom_res.xlsx', index=False, engine='openpyxl')
+# df.to_excel('./prom_res.xlsx', index=False, engine='openpyxl')
+df.to_csv(f'./{EXPORT_FILENAME}',index=False)
