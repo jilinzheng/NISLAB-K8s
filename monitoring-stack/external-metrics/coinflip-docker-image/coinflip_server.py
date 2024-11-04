fa1182f12d518ab787304a0f8c7a4c107fbb4991
Jilin Zheng <jilin@bu.edu>
2024-11-04 17:04:02 -0500

    updated external metrics server to serve avg pod utilization per deployment

--- monitoring-stack/external-metrics/coinflip-docker-image/coinflip_server.py
+++ monitoring-stack/external-metrics/coinflip-docker-image/coinflip_server.py
@@ -1,5 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask
 import random
+import requests
+
+# Prometheus APIs
+PROM_QUERY_INSTANT_API = "http://prometheus-service.monitoring.svc.cluster.local:9090/api/v1/query"
+PROM_QUERY_RANGE_API = "http://prometheus-service.monitoring.svc.cluster.local:9090/api/v1/query_range"
+# PROM_QUERY_INSTANT_API = "http://127.0.0.1:30000/api/v1/query"
+# PROM_QUERY_RANGE_API = "http://127.0.0.1:30000/api/v1/query_range"
+
+# list of target deployments
+TARGET_DEPLOYS = (['teastore-auth',
+                  'teastore-db',
+                  'teastore-image',
+                  'teastore-persistence',
+                  'teastore-recommender',
+                  'teastore-registry',
+                  'teastore-webui'])
+
+TARGET_DEPLOYS_UNDERSCORE = [deploy.replace('-','_') for deploy in TARGET_DEPLOYS]
+
+# average cpu utilization per pod of deployment
+def avg_cpu_per_pod_in_deploy(deployment):
+    return {"query":
+            f"""sum(
+                    rate(container_cpu_usage_seconds_total{{
+                        namespace="default",
+                        pod=~"{deployment}-.*"}}
+                        [1m]
+                        ))
+                /
+                scalar(
+                    count(
+                        kube_pod_info{{
+                            namespace="default",
+                            created_by_name=~"{deployment}-.*"
+                            }}))
+            """}
 
 app = Flask(__name__)
 
@@ -7,12 +43,30 @@ app = Flask(__name__)
 def index():
     return "Coinflip server up and running!"
 
+last_scale_response = ""
+
 @app.route('/metrics')
 def metrics():
-    # Simulate a coin flip: 0 or 1
+    global last_scale_response
+
+    # simulate a coin flip: 0 or 1
     coinflip_result = random.choice([0, 1])
-    # Return the result as a JSON response
-    return f"coinflip_result {coinflip_result}"
+
+    # if coinflip is 1/heads, return the proper scaling
+    if coinflip_result:
+        print(f"coinflip_result = {coinflip_result}")
+        last_scale_response = ""
+        for i, deploy in enumerate(TARGET_DEPLOYS):
+            res = requests.get(PROM_QUERY_INSTANT_API,
+                               params=avg_cpu_per_pod_in_deploy(deploy)).json()
+            last_scale_response += f"{TARGET_DEPLOYS_UNDERSCORE[i]} {res['data']['result'][0]['value'][1]}\n"
+        print(last_scale_response)
+        return last_scale_response
+
+    # if coinflip is 0/tails, return the last response to prevent another scale
+    else:
+        print(f"coinflip_result = {coinflip_result}")
+        return last_scale_response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
